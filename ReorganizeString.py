ca446e3b5bc69a06f367d2eef72ad48dde52e5ef
Fred Offei <112915741+danexpress@users.noreply.github.com>
2024-02-08 09:50:17 -0500

    reorgaize String

---
+++ ReorganizeString.py
@@ -0,0 +1,39 @@
+# Given a string s, rearrange the characters of s so that any two adjacent characters are not the same.
+
+# Return any possible rearrangement of s or return "" if not possible.
+
+
+# Example 1:
+
+# Input: s = "aab"
+# Output: "aba"
+# Example 2:
+
+# Input: s = "aaab"
+# Output: ""
+from collections import Counter
+import heapq
+
+
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        count = Counter(s)
+        maxHeap = [[-cnt, char] for char, cnt in count.items()]
+        heapq.heapify(maxHeap)
+
+        prev = None
+        res = ""
+        while maxHeap or prev:
+            if prev and not maxHeap:
+                return ""
+
+            cnt, char = heapq.heappop(maxHeap)
+            res += char
+            cnt += 1
+
+            if prev:
+                heapq.heappush(maxHeap, prev)
+                prev = None
+            if cnt != 0:
+                prev = [cnt, char]
+        return res
