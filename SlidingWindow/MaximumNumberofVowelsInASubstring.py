6f36b88b5027ea1333179bd0bd4dbe2c57fd8304
Fred Offei <112915741+danexpress@users.noreply.github.com>
2024-03-01 22:15:43 -0500

    maximum number

---
+++ SlidingWindow/MaximumNumberofVowelsInASubstring.py
@@ -0,0 +1,34 @@
+# Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+
+# Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
+
+
+# Example 1:
+
+# Input: s = "abciiidef", k = 3
+# Output: 3
+# Explanation: The substring "iii" contains 3 vowel letters.
+# Example 2:
+
+# Input: s = "aeiou", k = 2
+# Output: 2
+# Explanation: Any substring of length 2 contains 2 vowels.
+# Example 3:
+
+# Input: s = "leetcode", k = 3
+# Output: 2
+# Explanation: "lee", "eet" and "ode" contain 2 vowels.
+
+
+class Solution:
+    def maxVowels(self, s: str, k: int) -> int:
+        vowel = {"a", "e", "i", "o", "u"}
+
+        l, res, count = 0, 0, 0
+        for r in range(len(s)):
+            count += 1 if s[r] in vowel else 0
+            if r - l + 1 > k:
+                count -= 1 if s[l] in vowel else 0
+                l += 1
+            res = max(count, res)
+        return res
