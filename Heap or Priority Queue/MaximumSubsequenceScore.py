5ea3552d0bd93e63c935c78b38b4892e296fb99a
Fred Offei <112915741+danexpress@users.noreply.github.com>
2024-03-14 19:21:39 -0400

    maximim subsequence

--- Heap or Priority Queue/MaximumSubsequenceScore.py
+++ Heap or Priority Queue/MaximumSubsequenceScore.py
@@ -34,7 +34,7 @@ from typing import List
 
 class Solution:
     def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
-        pairs = [(n1, n2) for n1, n2 in zip(nums1, nums2)]
+        pairs = list(zip(nums1, nums2))
         pairs = sorted(pairs, key=lambda p: p[1], reverse=True)
 
         minHeap = []
