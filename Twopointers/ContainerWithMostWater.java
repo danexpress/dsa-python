ac74896bd67810f4488634896ad52d044cf5d630
Fred Offei <112915741+danexpress@users.noreply.github.com>
2024-03-22 22:36:08 -0400

    container with most water

---
+++ Twopointers/ContainerWithMostWater.java
@@ -0,0 +1,23 @@
+package Twopointers;
+
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int res = 0;
+
+        while (left < right) {
+            int area = (right - left) * Math.min(height[left], height[right]);
+            res = Math.max(res, area);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return res;
+    }
+
+}
