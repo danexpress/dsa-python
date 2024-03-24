a6e877dce1a90907700437e610b905aed2ecf15f
Fred Offei <112915741+danexpress@users.noreply.github.com>
2024-03-24 19:48:43 -0400

    find pivot index

---
+++ PrefixSum/FindPivotIndex.java
@@ -0,0 +1,22 @@
+package PrefixSum;
+
+import java.util.Arrays;
+
+public class FindPivotIndex {
+    public int pivotIndex(int[] nums) {
+        int total = Arrays.stream(nums).sum();
+        int left = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int right = total - left - nums[i];
+
+            if (right == left) {
+                return i;
+            }
+            left += nums[i];
+        }
+
+        return -1;
+    }
+
+}
