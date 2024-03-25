57a33c0e2ec0b4811b3dc404c59874a0b73ba2d3
Fred Offei <112915741+danexpress@users.noreply.github.com>
2024-03-24 20:05:47 -0400

    find the highest altitude

---
+++ PrefixSum/FindTheHighestAltitude.java
@@ -0,0 +1,14 @@
+package PrefixSum;
+
+public class FindTheHighestAltitude {
+    public int largestAltitude(int[] gain) {
+        int a = 0;
+        int res = 0;
+
+        for (int n : gain) {
+            a += n;
+            res = Math.max(res, a);
+        }
+        return res;
+    }
+}
