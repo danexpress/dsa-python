245ef5b29673fc4297730dc1dc9f36fd145d2138
Fred Offei <112915741+danexpress@users.noreply.github.com>
2024-03-28 18:53:57 -0400

    delete node

--- LinkedList/DeleteTheMiddleNode.java
+++ LinkedList/DeleteTheMiddleNode.java
@@ -1,5 +1,58 @@
 package LinkedList;
 
 public class DeleteTheMiddleNode {
+    public ListNode deleteMiddle(ListNode head) {
+        if (head == null)
+            return null;
+        if (head.next == null)
+            return null;
 
+        ListNode slow = head;
+        ListNode fast = head;
+
+        ListNode prev = null;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            prev = slow;
+            slow = slow.next;
+        }
+
+        prev.next = slow.next;
+        return head;
+
+    }
+}
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
 }
