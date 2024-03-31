7f47e6687ff6a062cb6fd32d1ff4cf8a338f0bb6
Fred Offei <112915741+danexpress@users.noreply.github.com>
2024-03-30 22:29:59 -0400

    reverse linked list

---
+++ LinkedList/ReverseLinkedList.java
@@ -0,0 +1,17 @@
+package LinkedList;
+
+public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode nextNode = null;
+        while (curr != null) {
+            nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+
+    }
+}
