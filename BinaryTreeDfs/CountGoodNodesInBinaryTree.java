0a883720ad6ce5ec3239526219eb408f0930c7ea
Fred Offei <112915741+danexpress@users.noreply.github.com>
2024-03-31 23:16:59 -0400

    count goo nodes in binary tree

---
+++ BinaryTreeDfs/CountGoodNodesInBinaryTree.java
@@ -0,0 +1,21 @@
+package BinaryTreeDfs;
+
+public class CountGoodNodesInBinaryTree {
+    int count = 0;
+
+    public int goodNodes(TreeNode root) {
+        helper(root, root.val);
+        return count;
+
+    }
+
+    private void helper(TreeNode root, int max) {
+        if (root != null) {
+            if (root.val >= max) {
+                count++;
+            }
+            helper(root.left, Math.max(root.val, max));
+            helper(root.right, Math.max(root.val, max));
+        }
+    }
+}
