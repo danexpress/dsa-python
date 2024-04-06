c52e2193e9ccf8f482db91bc439b7b571b34c8fe
Fred Offei <112915741+danexpress@users.noreply.github.com>
2024-04-05 20:40:19 -0400

    maximum level sum of a binary

---
+++ BinaryTreeBFS/MaximumLevelSum.java
@@ -0,0 +1,33 @@
+package BinaryTreeBFS;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MaximumLevelSum {
+    int mxSum = Integer.MIN_VALUE;
+    int minLevel = 1;
+
+    public int maxLevelSum(TreeNode root) {
+        int level = 1;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while (queue.size() > 0) {
+            int size = queue.size();
+            int sum = 0;
+            while (size-- > 0) {
+                TreeNode node = queue.remove();
+                sum += node.val;
+                if (node.left != null)
+                    queue.offer(node.left);
+                if (node.right != null)
+                    queue.offer(node.right);
+            }
+            if (sum > mxSum) {
+                minLevel = level;
+                mxSum = sum;
+            }
+            level++;
+        }
+        return minLevel;
+    }
+}
