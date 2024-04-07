7a504c61512c58f5391ba29c095d5c9e459ad572
Fred Offei <112915741+danexpress@users.noreply.github.com>
2024-04-06 22:17:50 -0400

    search in a binary search tree

---
+++ BinarySearchTree/SearchInABinarySearchTree.java
@@ -0,0 +1,18 @@
+package BinarySearchTree;
+
+public class SearchInABinarySearchTree {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null)
+            return null;
+        if (root.val == val)
+            return root;
+
+        if (val < root.val) {
+            return searchBST(root.left, val);
+
+        } else {
+            return searchBST(root.right, val);
+        }
+
+    }
+}
