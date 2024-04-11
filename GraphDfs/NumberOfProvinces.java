8dd217b7c3f3e3504901cc65b2042b24bed93b4a
Fred Offei <112915741+danexpress@users.noreply.github.com>
2024-04-10 22:08:58 -0400

    number of provinces

---
+++ GraphDfs/NumberOfProvinces.java
@@ -0,0 +1,29 @@
+package GraphDfs;
+
+public class NumberOfProvinces {
+    public int findCircleNum(int[][] isConnected) {
+        int numProvinces = isConnected.length;
+        boolean[] visited = new boolean[numProvinces];
+        int numCircles = 0;
+
+        for (int i = 0; i < numProvinces; i++) {
+            if (!visited[i]) {
+                numCircles++;
+                dfs(isConnected, i, visited);
+            }
+        }
+
+        return numCircles;
+    }
+
+    private void dfs(int[][] isConnected, int province, boolean[] visited) {
+
+        visited[province] = true;
+
+        for (int neighbour = 0; neighbour < isConnected[province].length; neighbour++) {
+            if (!visited[neighbour] && isConnected[province][neighbour] == 1) {
+                dfs(isConnected, neighbour, visited);
+            }
+        }
+    }
+}
