a193dd558f52d9fc55f91c1368181d7b8ee1351f
Fred Offei <112915741+danexpress@users.noreply.github.com>
2024-04-13 21:12:27 -0400

    smallest number in infinite set

---
+++ HeapOrPriorityQueue/SmallestNumberInInfiniteSet.java
@@ -0,0 +1,35 @@
+package HeapOrPriorityQueue;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SmallestNumberInInfiniteSet {
+    int cur;
+    Set<Integer> s;
+
+    public void SmallestInfiniteSet() {
+        cur = 1;
+        s = new HashSet<>();
+
+    }
+
+    public int popSmallest() {
+        if (!s.isEmpty()) {
+            int res = Collections.min(s);
+            s.remove(res);
+            return res;
+        } else {
+            cur++;
+            return cur - 1;
+        }
+
+    }
+
+    public void addBack(int num) {
+        if (cur > num) {
+            s.add(num);
+        }
+
+    }
+}
