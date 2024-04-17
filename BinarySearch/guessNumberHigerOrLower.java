bb54a032efbc8a37a21d48f2eb160d5089c663eb
Fred Offei <112915741+danexpress@users.noreply.github.com>
2024-04-17 19:02:35 -0400

    guess number

---
+++ BinarySearch/guessNumberHigerOrLower.java
@@ -0,0 +1,22 @@
+package BinarySearch;
+
+public class guessNumberHigerOrLower {
+    public int guessNumber(int n) {
+        int i = 1;
+        int j = n;
+
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+
+            int res = guess(mid);
+            if (res == 0)
+                return mid;
+            if (res == 1)
+                i = mid + 1;
+            else
+                j = mid - 1;
+        }
+
+        return -1;
+    }
+}
