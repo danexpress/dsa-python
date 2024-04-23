7a13ed7f2cc00f510e0b2f8e93a959bbd93ad230
Fred Offei <112915741+danexpress@users.noreply.github.com>
2024-04-23 18:18:49 -0400

    best time to buy and sell stock with transaction fee

---
+++ dp2/BestTimeToBuyAndSell.java
@@ -0,0 +1,16 @@
+package dp2;
+
+public class BestTimeToBuyAndSell {
+    public int maxProfit(int[] prices, int fee) {
+        int buy = Integer.MIN_VALUE;
+        int sell = 0;
+
+        for (int price : prices) {
+            buy = Math.max(buy, sell - price);
+            sell = Math.max(sell, buy + price - fee);
+        }
+
+        return sell;
+
+    }
+}
