963111d9a03244d29d3c550e00f206bbdf097cc9
Fred Offei <112915741+danexpress@users.noreply.github.com>
2024-04-26 21:44:46 -0400

    search suggestions system

---
+++ Trie/SearchSuggestionsSystem.java
@@ -0,0 +1,42 @@
+package Trie;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SearchSuggestionsSystem {
+    public List<List<String>> suggestedProductsTwoPointer(String[] products, String searchWord) {
+        Arrays.sort(products);
+
+        int left = 0;
+        int right = products.length - 1;
+
+        List<List<String>> result = new ArrayList<>();
+
+        for (int i = 0; i < searchWord.length(); i++) {
+
+            while (left <= right
+                    && (i >= products[left].length() || products[left].charAt(i) != searchWord.charAt(i))) {
+                left++;
+            }
+
+            while (left <= right
+                    && (i >= products[right].length() || products[right].charAt(i) != searchWord.charAt(i))) {
+                right--;
+            }
+
+            List<String> currentSuggestions = new ArrayList<>();
+
+            for (int j = left; j <= Math.min(right, left + 2); j++) {
+                currentSuggestions.add(products[j]);
+            }
+            result.add(currentSuggestions);
+        }
+        return result;
+    }
+
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        return suggestedProductsTwoPointer(products, searchWord);
+
+    }
+}
