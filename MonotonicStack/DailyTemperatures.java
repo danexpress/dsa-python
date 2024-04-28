a74a9d8a3b8881bfe45bdaebcfbd22285d18fb67
Fred Offei <112915741+danexpress@users.noreply.github.com>
2024-04-27 21:02:25 -0400

    dailytemperatures

---
+++ MonotonicStack/DailyTemperatures.java
@@ -0,0 +1,19 @@
+package MonotonicStack;
+
+import java.util.Stack;
+
+public class DailyTemperatures {
+    public int[] dailyTemperatures(int[] temperatures) {
+
+        int[] results = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < temperatures.length; i++) {
+            while (!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]) {
+                results[stack.peek()] = i - stack.pop();
+            }
+            stack.push(i);
+        }
+        return results;
+    }
+}
