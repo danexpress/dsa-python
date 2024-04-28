4d1053e87765261931efa66267c8877b3a601601
Fred Offei <112915741+danexpress@users.noreply.github.com>
2024-04-27 21:33:19 -0400

    online Stock Span

---
+++ MonotonicStack/StockSpanner.java
@@ -0,0 +1,25 @@
+package MonotonicStack;
+
+import java.util.Stack;
+
+public class StockSpanner {
+    Stack<int[]> st;
+
+    public StockSpanner() {
+        st = new Stack<>();
+
+    }
+
+    public int next(int price) {
+
+        int span = 1;
+
+        while (st.size() > 0 && price >= st.peek()[0]) {
+            span += st.pop()[1];
+        }
+
+        st.push(new int[] { price, span });
+
+        return span;
+    }
+}
