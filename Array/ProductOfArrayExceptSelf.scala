4bf6b581f3016cbfbcc3cd76f2e8b40e2400cf71
Fred Offei <112915741+danexpress@users.noreply.github.com>
2024-05-19 04:39:19 -0400

    revised product of except self scala

--- Array/ProductOfArrayExceptSelf.scala
+++ Array/ProductOfArrayExceptSelf.scala
@@ -1,18 +1,25 @@
 object Solution {
     def productExceptSelf(nums: Array[Int]): Array[Int] = {
-        val res = new Array[Int](nums.length)
-        val rlProducts = new Array[Int](nums.length)
+        val result = new Array[Int](nums.length)
+        result(0) = 1
 
-        rlProducts(nums.length -1) = nums(nums.length-1)
-        for (i <- nums.length - 2 to (0, step = -1)) {
-            rlProducts(i) = rlProducts(i + 1) * nums(i)
+        def iterateForward(index: Int, previous: Int): Unit = index match {
+            case i if i == nums.length => ()
+            case i =>
+            result(i) = result(i - 1) * previous
+            iterateForward(i + 1, nums(i))
         }
 
-        nums.indices.foldLeft(1) { case (leftProd, i) => 
-            val r = if (i < nums.length-1) rlProducts(i+1) else 1
-            res(i) = leftProd * r
-            leftProd * nums(i)
+        def iterateBackward(index: Int, previous: Int): Unit = index.match {
+            case i if i == -1 => ()
+            case i => 
+            result(i) = result(i) * previous
+            iterateBackward(i - 1, nums(i) * previous)
         }
-        res
-    }
+
+        iterateForward(1, nums(0))
+        iterateBackward(nums.length-1, 1)
+
+        result
+}
 }
