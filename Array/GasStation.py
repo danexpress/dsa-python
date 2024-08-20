120ad7c364b5c20a825db2ac5361404877530e80
Fred Offei <112915741+danexpress@users.noreply.github.com>
2024-08-20 00:59:02 -0400

    trapping rain water

--- Array/GasStation.py
+++ Array/GasStation.py
@@ -38,12 +38,14 @@ class Solution:
         if sum(gas) < sum(cost):
             return -1
 
-        tank = idx = 0
+        total = 0
+        idx = 0
 
         for i in range(len(gas)):
-            tank += gas[i] - cost[i]
+            total += gas[i] - cost[i]
 
-            if tank < 0:
-                tank, idx = 0, i + 1
+            if total < 0:
+                total = 0
+                idx = i + 1
 
         return idx
