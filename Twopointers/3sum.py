73158bb8327bbc5ade11d73fb760cc25128f9130
Fred Offei <112915741+danexpress@users.noreply.github.com>
2024-08-28 05:03:41 -0400

    3sum

---
+++ Twopointers/3sum.py
@@ -0,0 +1,94 @@
+# Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+# Notice that the solution set must not contain duplicate triplets.
+
+
+# Example 1:
+
+# Input: nums = [-1,0,1,2,-1,-4]
+# Output: [[-1,-1,2],[-1,0,1]]
+# Explanation:
+# nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+# nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+# nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+# The distinct triplets are [-1,0,1] and [-1,-1,2].
+# Notice that the order of the output and the order of the triplets does not matter.
+# Example 2:
+
+# Input: nums = [0,1,1]
+# Output: []
+# Explanation: The only possible triplet does not sum up to 0.
+# Example 3:
+
+# Input: nums = [0,0,0]
+# Output: [[0,0,0]]
+# Explanation: The only possible triplet sums up to 0.
+
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = set()
+
+        n, p, z = [], [], []
+        for num in nums:
+            if num > 0:
+                p.append(num)
+            elif num < 0:
+                n.append(num)
+            else:
+                z.append(num)
+
+        N, P = set(n), set(p)
+
+        if z:
+            for num in P:
+                if -1 * num in N:
+                    res.add((-1 * num, 0, num))
+
+        if len(z) >= 3:
+            res.add((0, 0, 0))
+
+        for i in range(len(n)):
+            for j in range(i + 1, len(n)):
+                target = -1 * (n[i] + n[j])
+                if target in P:
+                    res.add(tuple(sorted([n[i], n[j], target])))
+
+        for i in range(len(p)):
+            for j in range(i + 1, len(p)):
+                target = -1 * (p[i] + p[j])
+                if target in N:
+                    res.add(tuple(sorted([p[i], p[j], target])))
+
+        return res
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            j = i + 1
+            k = len(nums) - 1
+
+            while j < k:
+                total = nums[i] + nums[j] + nums[k]
+
+                if total > 0:
+                    k -= 1
+                elif total < 0:
+                    j += 1
+                else:
+                    res.append([nums[i], nums[j], nums[k]])
+                    j += 1
+
+                    while nums[j] == nums[j - 1] and j < k:
+                        j += 1
+
+        return res
